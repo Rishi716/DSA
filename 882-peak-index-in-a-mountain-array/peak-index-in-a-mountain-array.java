5458d00293efa514bdf88686d1609045a3795342
Adarsh Dutta <adarshdutta30@gmail.com>
2026-08-07 16:15:52 +0530

    Time: 1 ms (13.34%) | Memory: 80.2 MB (78.68%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n=arr.length;
+
+            //adarsh
+             for(int i=0;i<n-1;i++){
+                if(arr[i]>arr[i+1]) {
+                return i;
+                }
+
+        } return -1;
+    }
+}
