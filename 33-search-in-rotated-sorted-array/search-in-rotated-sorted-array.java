965c348e9b3438fd6e293d220a08b4f2a19201de
Adarsh Dutta <adarshdutta30@gmail.com>
2026-08-23 18:06:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (66.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,32 +1,22 @@
 class Solution {
-    public int search(int[] arr, int target) {
+    public int search(int[] arr, int tar) {
         int n= arr.length;
-        int pivot =0;
-        for(int i=1;i<n-1;i++){
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]) pivot=i;
-        }
-    
-     int l=0,h=pivot ;
-    
+        int l=0,h=n-1;
+        
         while(l<=h){
-            int mid= (l+h)/2;
-            if(target < arr[mid]) h=mid-1;
-                  else  if(target == arr[mid]) 
-                     return mid;
-    
-            else l=mid+1;
-
-        } 
-          l=pivot+1
-          ;h=n-1 ;
-    
-        while(l<=h){
-            int mid= (l+h)/2;
-            if(target <arr[mid]) 
-                h=mid-1;
-            else if (target > arr[mid])  l=mid+1;
-            else return mid;
-        } 
-        return -1;
+        int mid=l+(h-l)/2;
+    if(arr[mid]==tar) return mid;
+    else if (arr[l]<=arr[mid]){
+          if(arr[l]<=tar && tar<arr[mid]) h=mid-1;
+          else l=mid+1;
+       }
+    else {
+         if(arr[mid]<tar && tar<=arr[h]) l=mid+1;
+         else h= mid-1;
     }
+    }
+    return -1;
+    }
+    
+  
 }
