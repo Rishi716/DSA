aafe94ddd00b5cf431528ff458c99236057902ed
Adarsh Dutta <adarshdutta30@gmail.com>
2026-08-29 04:14:11 +0530

    Time: 14 ms (28.57%) | Memory: 46 MB (33.72%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+
+        int n = s.length();
+        int i = 0;
+        int j = n - 1;  
+
+        while(i <= j) {
+            if(s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            }
+            else return false;
+        }
+
+        return (i > j);
+    }
+}
