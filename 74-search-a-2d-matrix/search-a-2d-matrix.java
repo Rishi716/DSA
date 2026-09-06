483afbe91b9ef3a7c70157d68802da81e0b547d0
Adarsh Dutta <adarshdutta30@gmail.com>
2026-09-06 12:19:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (62.24%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -2,13 +2,15 @@ class Solution {
     public boolean searchMatrix(int[][] arr, int tar) {
         int rows= arr.length;
         int cols= arr[0].length;
-        int l=0,h=rows*cols-1;
+        int l=0,h=rows*cols-1; // like n-1 in 1d array.D
         
 
         while (l<=h){
             int mid=l+(h-l)/2;
+// imp concept 
             int midr=mid/cols;
              int midc=mid % cols;
+
             if(tar == arr[midr][midc]) return true;
             else if(tar < arr[midr][midc] ) h=mid-1;
             else l=mid+1;
